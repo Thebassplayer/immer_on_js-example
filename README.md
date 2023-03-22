005393192c1728b621baa9cea0b76fbc13b80c90
Thebassplayer <ryhsu2@gmail.com>
2023-03-22 13:22:22 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# immer_on_js-example
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-g57szg)
---
+++ index.js
@@ -0,0 +1,15 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+const obj1 = {
+  firstName: 'Romina',
+  lastName: 'Quiroga',
+  parents: {
+    mother: 'Estela',
+    father: 'Sergio',
+  },
+};
